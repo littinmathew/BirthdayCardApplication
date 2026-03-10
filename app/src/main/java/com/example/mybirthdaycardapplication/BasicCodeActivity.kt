5a9e84e023a81005f97813c2388554d9a383909b
Littin <littin@itware.ae>
2026-03-10 11:31:39 +0400

    add button to row

--- app/src/main/java/com/example/mybirthdaycardapplication/BasicCodeActivity.kt
+++ app/src/main/java/com/example/mybirthdaycardapplication/BasicCodeActivity.kt
@@ -51,7 +51,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello")
                 Text(text = name)
             }
